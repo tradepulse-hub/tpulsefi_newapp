ecae02b22a745effa58dd13d06092ead82e5c62e
tradepulse-hub <support@tradepulsetoken.com>
2025-07-23 20:59:18 +0100

    Update route.ts

--- app/api/nonce/route.ts
+++ app/api/nonce/route.ts
@@ -7,12 +7,7 @@ export function GET() {
 
   // The nonce should be stored somewhere that is not tamperable by the client
   // Optionally you can HMAC the nonce with a secret key stored in your environment
-  cookies().set("siwe", nonce, {
-    secure: process.env.NODE_ENV === "production", // Use secure only in production
-    httpOnly: true, // Boa prática para cookies de nonce
-    sameSite: "lax", // CORRIGIDO: Garante que o cookie seja enviado em navegações de nível superior
-    path: "/", // CORRIGIDO: Garante que o cookie esteja disponível em toda a aplicação
-  })
+  cookies().set("siwe", nonce, { secure: true })
 
   return NextResponse.json({ nonce })
 }
